052515d0c7d4ad3576650dcdd303b95eabdea240
DESKTOP22 <pshivkanya414@gmail.com>
2024-05-16 18:23:08 +0530

    testcase for sum of string

--- string_calculator.rb
+++ string_calculator.rb
@@ -18,4 +18,8 @@ class TestStringCalculator < Minitest::Test
   def test_returns_the_number_itself_for_a_single_number_string
     assert_equal 1, StringCalculator.add("1")
   end
+
+  def test_returns_the_sum_of_numbers_for_comma_separated_numbers
+    assert_equal 6, StringCalculator.add("1,2,3")
+  end
 end

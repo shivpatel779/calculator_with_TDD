62c9a335097ce8fa7e79261bd7234e524eef1d27
DESKTOP22 <pshivkanya414@gmail.com>
2024-05-16 18:42:08 +0530

    testcase for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -48,4 +48,13 @@ class TestStringCalculator < Minitest::Test
   def test_supports_custom_delimiters
     assert_equal 3, StringCalculator.add("//;\n1;2")
   end
+
+  def test_raises_exception_for_negative_numbers
+    assert_raises(RuntimeError) { StringCalculator.add("2,-3") }
+  end
+
+  def test_exception_message_includes_all_negative_numbers
+    error = assert_raises(RuntimeError) { StringCalculator.add("-1,2,-3,-4") }
+    assert_equal "Negative numbers not allowed: -1, -3, -4", error.message
+  end
 end

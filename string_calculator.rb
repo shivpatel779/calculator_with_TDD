68e12923ad92114ad214d99e8c888695bb5bdb13
DESKTOP22 <pshivkanya414@gmail.com>
2024-05-16 18:11:43 +0530

    method for empty string

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,15 @@
 require 'minitest/autorun'
+require "byebug"
+
+class StringCalculator
+
+  def self.add(numbers)
+    if numbers.empty?
+      return 0
+    end
+  end
+end
+
 class TestStringCalculator < Minitest::Test
 
   def test_returns_0_for_an_empty_string
